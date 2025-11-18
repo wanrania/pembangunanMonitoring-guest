41a315d405eb02c450d747651890e3452c367e0d
wanrania <wan24si@mahasiswa.pcr.ac.id>
2025-11-18 13:08:26 +0700

    ralat

--- resources/views/pages/guest/lokasi/index.blade.php
+++ resources/views/pages/guest/lokasi/index.blade.php
@@ -4,9 +4,9 @@
 <main class="container mt-5">
 
     <div class="d-flex justify-content-between align-items-center mb-4">
-            <h3 class="fw-bold text-primary">Daftar Proyek</h3>
+            <h3 class="fw-bold text-primary">Daftar Lokasi</h3>
             <a href="{{ route('lokasi.create') }}" class="btn btn-primary">
-                <i class="fa-solid fa-plus me-1"></i> Tambah Proyek
+                <i class="fa-solid fa-plus me-1"></i> Tambah Lokasi
             </a>
 
         </div>
@@ -34,12 +34,12 @@
                 <td>{{ $item->lng }}</td>
                 <td>{{ $item->geojason }}</td>
 
-                <div class="card-footer bg-transparent border-0 d-flex justify-content-between p-3">
+                {{-- <div class="card-footer bg-transparent border-0 d-flex justify-content-between p-3">
                             <a href="{{ route('lokasi.edit', $item->lokasi_id) }}" class="btn btn-warning btn-sm">
                                 <i class="fa-solid fa-pen-to-square me-1"></i> Edit
                             </a>
 
-                            <form action="{{ route('lokasi.destroy', $item->lokasi_id) }}" method="POST"
+                            <form action="{{ route('proyek.destroy', $proyek->proyek_id) }}" method="POST"
                                 style="display:inline;">
                                 @csrf
                                 @method('DELETE')
@@ -48,7 +48,20 @@
                                 </button>
                             </form>
 
-                        </div>
+                        </div> --}}
+                 <td>
+                    <a href="{{ route('lokasi.edit', $item->lokasi_id) }}" class="btn btn-warning btn-sm">
+                                <i class="fa-solid fa-pen-to-square me-1"></i> Edit
+                            </a>
+
+                    <form action="{{ route('lokasi.destroy', $item->lokasi_id) }}" method="POST" style="display:inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">
+                                    <i class="fa-solid fa-trash-can me-1"></i> Hapus
+                                </button>
+                    </form>
+                </td>
             </tr>
             @endforeach
         </tbody>
